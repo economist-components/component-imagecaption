bb61f562defb9bcca6e95726a617d3b39daed566
umbobabo <umbertobabini@economist.com>
2015-07-06 17:37:20 +0100

    Link ref on package

--- example.js
+++ example.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ImageCaption from './';
 
 export default (
-  <ImageCaption caption="My caption" src="http://lorempixel.com/g/300/169/food" >
-  </ImageCaption>
+  <ImageCaption caption="My caption" src="http://lorempixel.com/g/300/169/food" ></ImageCaption>
 );
 
